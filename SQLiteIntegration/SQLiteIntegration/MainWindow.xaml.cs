fa25acae1404c24e9d0f19daa917af83e31f5e72
Otto Borchert <otto.borchert@ndsu.edu>
2020-04-08 15:33:08 -0500

    Base commit for students to install SQLite and fix the errors.

---
+++ SQLiteIntegration/SQLiteIntegration/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SQLiteIntegration
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (HasValidInput())
+            {
+                int doubles = int.Parse(DoublesTextBox.Text);
+                int triples = int.Parse(TriplesTextBox.Text);
+
+                //MARKER - Probably an error here
+                DataTable dt = new DataTable();
+
+                string datasource = @"Data Source=../../lahman2016.sqlite;";
+                //Batting.'2B' is the number of doubles a player hit in a season
+                //Batting.'3B' is the number of triples a player hit in a season
+                string sql = $"SELECT playerID, Batting.'2B' FROM Batting GROUP BY Batting.playerID HAVING SUM(Batting.'2B') > {doubles};";
+                using (SQLiteConnection conn = new SQLiteConnection(datasource))
+                {
+                    conn.Open();
+                    SQLiteDataAdapter da = new SQLiteDataAdapter(sql, conn);
+                    da.Fill(dt);
+                    conn.Close();
+                }
+                playerList.Clear();
+                foreach (DataRow row in dt.Rows)
+                {
+                    // MARKER: Making something different show up
+                    string playerRow = $"{row[0].ToString()}";
+                    playerList.Add(playerRow);
+                }
+                populateList();
+            }
+        }
+
+        private void populateList()
+        {
+            ListView.Items.Clear();
+            foreach (string s in playerList)
+            {
+                ListView.Items.Add(s);
+            }
+        }
+
+        private string ValidateTextBox(TextBox box, string name)
+        {
+            string message = "";
+            string text = box.Text;
+            int amount = 0;
+
+            if (text == "")
+                box.Text = "0";
+            else
+            {
+                bool isNumber = int.TryParse(text, out amount);
+                if (!isNumber)
+                {
+                    message += $"Invalid Input - {name} is not a number! ";
+                }
+                if (amount < 0)
+                {
+                    message += $"Invalid Input - {name} is negative! ";
+                }
+            }
+            return message;
+        }
+
+        private bool HasValidInput()
+        {
+            string message = ValidateTextBox(DoublesTextBox, "Doubles") +
+                ValidateTextBox(TriplesTextBox, "Triples");
+
+            if (message == "")
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show(message);
+                return false;
+            }
+        }
+    }
+}
