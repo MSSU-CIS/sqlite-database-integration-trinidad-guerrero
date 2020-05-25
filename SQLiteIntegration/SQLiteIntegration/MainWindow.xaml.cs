99ab121676069388b8b7f0057b7e426ae477b7ea
Tino <60246343+guerrtrn@users.noreply.github.com>
2020-05-25 09:56:04 -0500

    Final 5/25/20

--- SQLiteIntegration/SQLiteIntegration/MainWindow.xaml.cs
+++ SQLiteIntegration/SQLiteIntegration/MainWindow.xaml.cs
@@ -12,91 +12,100 @@ namespace SQLiteIntegration
     /// </summary>
     public partial class MainWindow : Window
     {
-      List<string> playerList = new List<string>();
+      List<string> SalariesList = new List<string>();
       public MainWindow()
-        {
+      {
 
-        }
+      }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (HasValidInput())
+      private void Button_Click(object sender, RoutedEventArgs e)
+      {
+         if (HasValidInput())
+         {
+            int BasePay = int.Parse(BasePayTextBox.Text);
+            string EmployeeName = EmployeeNameTextBox.Text;
+
+            //MARKER - Probably an error here
+            DataTable dt = new DataTable();
+
+            string datasource = @"Data Source=../../database1.sqlite;";
+            //string datasource = @"Data Source=../../whatcd-hiphop.sqlite;";
+            //Batting.'2B' is the number of torrents a player hit in a season
+            //Batting.'3B' is the number of tags a player hit in a season
+            string sql = $"SELECT EmployeeName, JobTitle, Description, Sum (Salaries.BasePay) As BasePay,Sum (Salaries.OverTimePay),Sum (Salaries.Benefits),Sum (Salaries.TotalPay),Sum (Salaries.TotalPayBenefits) FROM Salaries INNER JOIN Jobs ON Salaries.JobTitle = Jobs.Job Where EmployeeName Like '%{EmployeeName}%' GROUP BY Salaries.EmployeeName, Salaries.JobTitle, Jobs.Description HAVING Sum (Salaries.BasePay) >= {BasePay};";
+            using (SQLiteConnection conn = new SQLiteConnection(datasource))
             {
-                int doubles = int.Parse(DoublesTextBox.Text);
-                int triples = int.Parse(TriplesTextBox.Text);
+               conn.Open();
+               SQLiteDataAdapter da = new SQLiteDataAdapter(sql, conn);
+               da.Fill(dt);
+               conn.Close();
+            }
 
-                //MARKER - Probably an error here
-                DataTable dt = new DataTable();
-
-                string datasource = @"Data Source=../../lahman2016.sqlite;";
-                //Batting.'2B' is the number of doubles a player hit in a season
-                //Batting.'3B' is the number of triples a player hit in a season
-                string sql = $"SELECT namefirst, namelast,Sum (Batting.'2B'),Sum (Batting.'3B') FROM Master INNER JOIN Batting ON Batting.playerid = Master.playerid GROUP BY Master.playerid HAVING Sum (Batting.'2B') > {doubles} AND Sum (Batting.'3B')> {triples};";
-                using (SQLiteConnection conn = new SQLiteConnection(datasource))
-                {
-                    conn.Open();
-                    SQLiteDataAdapter da = new SQLiteDataAdapter(sql, conn);
-                    da.Fill(dt);
-                    conn.Close();
-                }
-
-                playerList.Clear();
-                foreach (DataRow row in dt.Rows)
-                {
+            SalariesList.Clear();
+            foreach (DataRow row in dt.Rows)
+            {
                // MARKER: Making something different show up
-               string playerRow = $"{row​[0].ToString()} {row​[1].ToString()} - 2B = {row​[2].ToString()}, 3B = {row​[3].ToString()}";
-               playerList.Add(playerRow);
-                }
-                populateList();
+               string EmployeeRow = $"{row​[0].ToString()}, {row​[1].ToString()} - {row​[2].ToString()}, {row​[3].ToString()}";
+               SalariesList.Add(EmployeeRow);
             }
-        }
+            populateList();
+         }
+      }
 
-        private void populateList()
-        {
-            ListView.Items.Clear();
-            foreach (string s in playerList)
+      private void populateList()
+      {
+         SalariesView.Items.Clear();
+         foreach (string s in SalariesList)
+         {
+            SalariesView.Items.Add(s);
+         }
+      }
+
+      private string ValidateTextBox(TextBox box, string name)
+      {
+         string message = "";
+         string text = box.Text;
+         int amount = 0;
+
+         if (text == "")
+            box.Text = "0";
+         else
+         {
+            if (name.Equals("EmployeeName"))
             {
-                ListView.Items.Add(s);
-            }
-        }
-
-        private string ValidateTextBox(TextBox box, string name)
-        {
-            string message = "";
-            string text = box.Text;
-            int amount = 0;
-
-            if (text == "")
-                box.Text = "0";
-            else
-            {
-                bool isNumber = int.TryParse(text, out amount);
-                if (!isNumber)
-                {
-                    message += $"Invalid Input - {name} is not a number! ";
-                }
-                if (amount < 0)
-                {
-                    message += $"Invalid Input - {name} is negative! ";
-                }
-            }
-            return message;
-        }
-
-        private bool HasValidInput()
-        {
-            string message = ValidateTextBox(DoublesTextBox, "Doubles") +
-                ValidateTextBox(TriplesTextBox, "Triples");
-
-            if (message == "")
-            {
-                return true;
+               if (text.Equals(""))
+                  message += $"Invalid Input - {name} cannot be empty! ";
             }
             else
             {
-                MessageBox.Show(message);
-                return false;
+               bool isNumber = int.TryParse(text, out amount);
+               if (!isNumber)
+               {
+                  message += $"Invalid Input - {name} is not a number! ";
+               }
+               if (amount < 0)
+               {
+                  message += $"Invalid Input - {name} is negative! ";
+               }
             }
-        }
-    }
+         }
+         return message;
+      }
+
+      private bool HasValidInput()
+      {
+         string message = ValidateTextBox(EmployeeNameTextBox, "EmployeeName") +
+             ValidateTextBox(BasePayTextBox, "BasePay");
+
+         if (message == "")
+         {
+            return true;
+         }
+         else
+         {
+            MessageBox.Show(message);
+            return false;
+         }
+      }
+   }
 }
